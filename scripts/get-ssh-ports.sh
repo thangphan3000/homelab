10d327c1d0e601280194ed2a9624ac8d99c5e007
Thang Phan <thangjenny2002@gmail.com>
2025-05-24 16:08:50 +0700

    feat(k8s): install kuberenetes components

--- scripts/get-ssh-ports.sh
+++ scripts/get-ssh-ports.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vagrant global-status | grep vmware_desktop | awk '{print $2}' | while read -r vm_name; do
+vagrant global-status | grep virtualbox | awk '{print $2}' | while read -r vm_name; do
   host_port=$(vagrant port "$vm_name" | tail -1 | awk '{print $4}')
   echo "Virtual machine name: $vm_name, ssh port: $host_port"
 done
